b0ae044cf018d9e5c076584fe52dc99e6e957f68
DindoW <7yearschest@gmail.com>
2017-04-12 21:58:01 +0800

    OpenMP并行函数库
    C++、OpenMP实现并行排序、矩阵乘法、线性方程组求解、傅里叶变换。

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include "omplib.h"
+#include <time.h>
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+int main()
+{
+	clock_t pt = 0, st = 0;
+	int n = 1000000, p = 4;
+
+
+	int *a = new int[n];
+	int *b = new int[n];
+	int *c = new int[n];
+
+	srand((unsigned)time(NULL));
+	//	for (int i = 0; i < n; i++)
+	//		a[i] = (double)rand() / (RAND_MAX / 10);
+	for (int i = 0; i < n; i++)
+		a[i] = i;
+	random_shuffle(a, a + n);
+	for (int i = 0; i < n; i++)
+		b[i] = c[i] = a[i];
+
+	cout << "A: ";
+	//	for (int i = 0; i < n; i++)
+	//		cout << a[i] << "  ";
+	cout << endl;
+
+	//*****串行排序时间
+	{
+		st -= clock();
+		//		sort(b, b + n, greater<double>());
+		sort(b, b + n);
+		st += clock();
+	}
+
+	//******并行排序时间
+	{
+		pt -= clock();
+		psort(c, n, p);
+		pt += clock();
+	}
+
+	//*******验证排序结果
+	cout << endl << "B: ";
+	//	for (int i = 0; i < n; i++)
+	//		cout << b[i] << "  ";
+	cout << endl << "C: ";
+	for (int i = 0; i < n; i++)
+	{
+		if (b[i] != (double)c[i])
+		{
+			cout << "e:";
+			break;
+		}
+		//		cout << (double)c[i] << "  ";
+	}
+	cout << endl;
+
+	delete[]a;
+	delete[]b;
+	delete[]c;
+
+
+
+
+
+
+
+
+	printf("serial time: %lf s.\n", (double)st / CLOCKS_PER_SEC);
+	printf("parallel time: %lf s.\n", (double)pt / CLOCKS_PER_SEC);
+	printf("speedup ratio: %lf \n", (double)st / pt);
+
+	system("pause");
+	return 0;
+}
---
+++ omplib.h
@@ -0,0 +1,233 @@
+#pragma once
+#include <omp.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <assert.h>
+
+//#define DEBUG
+#ifdef DEBUG	//*******输出调试信息
+#include <iostream>
+#include <iomanip>
+#endif // DEBUG
+
+template <typename T> void psort(T *arr, int n, int p, int sortType = 1)
+{
+	/*
+	*(1)均匀划分: n个元素均匀地划分成p段，每台处理器有n/p个元素
+	*(2)局部排序: 各处理器利用串行排序算法，排序n/p个数
+	*(3)正则采样: 每台处理器各从自己的有序段中选取p个样本元素(各段的第1、n/p^2+1、2n/p^2+1、…个元素)
+	*(4)样本排序: 用一台处理器将所有p^2个样本元素用串行排序算法排序之
+	*(5)选择主元: 用一台处理器选取p-1个主元，并将其播送给其余处理器(第p+1、2p+1、…个元素)
+	*(6)主元划分: 各处理器按主元将各自的有序段划分成p段
+	*(7)全局交换: 各处理器将其辖段按段号交换到对应的处理器中
+	*(8)归并排序: 各处理器使用归并排序将所接收的诸段施行排序
+	*/
+	assert(arr != NULL);
+	assert(n % p == 0);
+	assert(n > 0);
+
+	int len = n / p;
+	int square_p = p * p;
+	int gap = n / square_p;
+
+	T *tmp = new T[n];
+	T *pivot_element = new T[p];		//储存主元,p-1个
+	int **sgm_pvt_elm = new int*[p];	//储存主元划分的位置;
+	for (int i = 0; i < p; i++)
+		sgm_pvt_elm[i] = new int[p + 1];
+	int *mergeIndx = new int[p + 1];	//全局交换归并排序每个处理器排序后的总长度
+	mergeIndx[p] = 0;
+
+	omp_set_num_threads(p);
+#pragma omp parallel shared(arr, tmp, pivot_element, sgm_pvt_elm, mergeIndx)
+	{
+		int i, j, k;
+		int tid = omp_get_thread_num();
+		/***********2：局部排序*********/
+		int startPos = tid * len;	//每段的起始位置
+		std::sort(arr + startPos, arr + startPos + len);
+#ifdef DEBUG	//**********测试输出
+#pragma omp single
+		for (i = 0; i < p; i++)
+		{
+			for (j = i * len; j < (i + 1) * len; j++)
+				std::cout << setw(4) << arr[j];
+		}
+#endif // DEBUG
+
+		/***********3：正则采样**********/
+		k = tid * p;		//每段的样本的起始存储位置
+		j = 0;
+		for (i = startPos; i < startPos + len; i += gap)
+		{
+			tmp[k + j] = arr[i];		//每段选出P个样本
+			j++;
+		}
+#ifdef DEBUG	//**********测试输出
+#pragma omp single
+		{
+			for (i = 0; i < p; i++)
+			{
+				std::cout << endl << i << "tid: ";
+				for (j = 0; j < p; j++)
+					std::cout << setw(4) << tmp[i * p + j];
+			}
+		}
+#endif // DEBUG
+#pragma omp barrier
+#pragma omp single
+		{
+			/*********4：样本排序**********/
+			std::sort(tmp, tmp + square_p);
+#ifdef DEBUG	//**********测试输出
+			std::cout << endl << "sort of sample: " << endl;
+			for (k = 0; k < square_p; k++)
+				std::cout << setw(4) << tmp[k];
+#endif // DEBUG
+			/*********5：选择主元**********/
+			for (i = p, j = 0; i < square_p; i += p)	//用一台处理器选取p-1个主元
+				pivot_element[j++] = tmp[i];
+#ifdef DEBUG	//**********测试输出
+			std::cout << endl << "pivot_element: " << endl;
+			for (j = 0; j < p - 1; j++)	//共有p-1个主元
+				std::cout << setw(4) << pivot_element[j];
+#endif // DEBUG
+		}
+		/*********6：主元划分**********/
+		sgm_pvt_elm[tid][0] = startPos;	//各个处理器处理的首位置，为第一个位置的前一位
+		sgm_pvt_elm[tid][p] = startPos + len;	//末尾位置
+		for (j = 0, k = startPos; j < p - 1; j++)		//各处理器按p-1个主元将各自的有序段划分成p段
+		{
+			for (; k < startPos + len; k++)	//找到相应位置存储下标
+			{
+				if (arr[k] > pivot_element[j])
+					break;
+			}
+			sgm_pvt_elm[tid][j + 1] = k;	//存储主元划分的位置
+		}
+#ifdef DEBUG	//**********测试输出
+#pragma omp barrier
+#pragma omp single
+		{
+			for (i = 0; i < p; i++)
+			{
+				std::cout << endl << "sgm_pvt_elm_pos_threads: " << i << endl;
+				for (j = 0; j < p + 1; j++)
+					std::cout << setw(4) << sgm_pvt_elm[i][j];
+			}
+		}
+#endif // DEBUG		
+		/************7全局交换**************/
+		mergeIndx[tid] = 0;		//merge_pos[p] = 0; 申请空间时处理，最后一个元素为0
+#pragma omp barrier
+		for (i = 0; i < p; i++)	//每个处理器需要归并p段
+		{	//各处理器将其辖段按段长度交换，计算出新的辖段长度，即每个线程归并排序需要的总长度
+			mergeIndx[tid + 1] += (sgm_pvt_elm[i][tid + 1] - sgm_pvt_elm[i][tid]); //因为储存位置，计算长度不需要加1
+		}
+#pragma omp barrier
+#pragma omp single
+		{
+#ifdef DEBUG	//*******测试输出
+			std::cout << endl << "merge_length: " << endl;
+			for (i = 0; i < p + 1; i++)
+				std::cout << setw(4) << mergeIndx[i];
+#endif // DEBUG
+			for (i = 1; i <= p; i++)
+				mergeIndx[i] = mergeIndx[i] + mergeIndx[i - 1];	//每个处理器归并结果存放区域	
+#ifdef DEBUG	//*******测试输出
+			std::cout << endl << "merge_sort_begin_at: " << endl;
+			for (i = 0; i < p + 1; i++)
+				std::cout << setw(4) << mergeIndx[i];
+			std::cout << endl << "A: ";
+			for (i = 0; i < n; i++)
+				std::cout << setw(3) << arr[i];
+#endif // DEBUG
+		}
+		/************8归并排序**************/
+		T max;
+		int maxIndex, ps;
+		int *index = new int[p];
+		int *endIndex = new int[p];
+
+		for (i = 0; i < p; i++)
+		{
+			index[i] = sgm_pvt_elm[i][tid];
+			endIndex[i] = sgm_pvt_elm[i][tid + 1] - 1;
+		}
+
+		max = arr[endIndex[0]];
+		maxIndex = 0;
+		for (i = 1; i < p; i++)
+		{
+			if (arr[endIndex[i]] >= max)
+			{
+				max = arr[endIndex[i]];
+				maxIndex = i;
+			}
+		}	//得到maxNumber和maxIndex
+#ifdef DEBUG	//**********测试输出
+#pragma omp critical
+		{
+			std::cout << endl << "tid::" << tid;
+			for (i = 0; i < p; i++)
+			{
+				std::cout << endl << "NO." << i << " from:" << index[i] << " to:" << endIndex[i] << endl;
+				for (j = index[i]; j <= endIndex[i]; j++)
+					std::cout << setw(4) << arr[j];
+			}
+			std::cout << endl << "max = " << max << " maxIndex = " << maxIndex;
+		}
+#endif // DEBUG
+
+		for (i = mergeIndx[tid]; i < mergeIndx[tid + 1]; i++)	//p路归并存放位置
+		{
+			T m = max;
+			ps = maxIndex;
+			for (j = 0; j < p; j++)	//从p段中找出最小的
+			{
+				if (arr[index[j]] <= m && index[j] <= endIndex[j])
+				{
+					m = arr[index[j]];	//记录最小值
+					ps = j;		//记录最小值所在的tid
+				}
+			}
+			tmp[i] = m;
+			index[ps]++;		//下次不比较该值
+		}
+		delete[] index;
+		delete[] endIndex;
+#pragma omp barrier
+#ifdef DEBUG	//**********测试输出
+#pragma omp single
+		{
+			std::cout << endl << "mergeIndx: ";
+			for (i = 0; i <= p; i++)
+				std::cout << setw(4) << mergeIndx[i];
+		}
+#pragma omp critical
+		{
+			std::cout << endl << "tid" << tid
+				<< "  start:" << mergeIndx[tid] << "  end:" << mergeIndx[tid + 1] << endl;
+			for (i = mergeIndx[tid]; i < mergeIndx[tid + 1]; i++)
+				std::cout << setw(4) << tmp[i];
+		}
+#endif // DEBUG
+		if (sortType)
+		{
+			for (i = startPos; i < startPos + len; i++)
+				arr[i] = tmp[i];
+		}
+		else
+		{
+			for (i = startPos; i < startPos + len; i++)
+				arr[n - 1 - i] = tmp[i];
+		}
+	}
+	for (int i = 0; i < p; i++)
+		delete[] sgm_pvt_elm[i];
+	delete[] sgm_pvt_elm;
+	delete[] pivot_element;
+	delete[] mergeIndx;
+	delete[] tmp;
+}
+
